b84865e9539cb25bacd1ceae74d5ab889217caba
Minh Nguyen <nhminh0105@gmail.com>
2021-04-30 12:41:57 +0700

    commit 3

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
 export const test = {
     key: "commit1",
-    key2: "commit2"
-}
-export const masterCommit = {
+    key2: "commit2",
+    key3: "commit3"
+}export const masterCommit = {
     key: "test"
 }
